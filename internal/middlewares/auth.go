a49af261afe78cb8150c03439456d8d07fe7ec72
João Vitor Bonadiman <18357636+jbonadiman@users.noreply.github.com>
2023-04-29 18:41:14 -0300

    adds more logging...

--- internal/middlewares/auth.go
+++ internal/middlewares/auth.go
@@ -2,12 +2,20 @@ package middlewares
 
 import (
 	"errors"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
 func Auth(w http.ResponseWriter, r *http.Request) error {
+	log.Println("dumping headers...")
+	for name, values := range r.Header {
+		for _, value := range values {
+			log.Printf("%s: %s\n", name, value)
+		}
+	}
+
 	secret := strings.TrimSpace(os.Getenv("API_TOKEN"))
 
 	if r.Header.Get("Authorization") != "Bearer "+secret {
