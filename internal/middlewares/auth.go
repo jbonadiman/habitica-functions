ec2c70cab89cbac874d0aa1636ad8c64722ebe43
João Vitor Bonadiman <18357636+jbonadiman@users.noreply.github.com>
2023-04-29 18:46:50 -0300

    remove headers logging...

--- internal/middlewares/auth.go
+++ internal/middlewares/auth.go
@@ -2,20 +2,12 @@ package middlewares
 
 import (
 	"errors"
-	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
 func Auth(w http.ResponseWriter, r *http.Request) error {
-	log.Println("dumping headers...")
-	for name, values := range r.Header {
-		for _, value := range values {
-			log.Printf("%s: %s\n", name, value)
-		}
-	}
-
 	secret := strings.TrimSpace(os.Getenv("API_TOKEN"))
 
 	if r.Header.Get("Authorization") != "Bearer "+secret {
